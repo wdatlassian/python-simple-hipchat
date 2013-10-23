2cdea3971872d505f3c3aac3254759510f1a2b0a
Jason Bartz <jrbartz@gmail.com>
2013-10-23 18:15:05 -0400

    Added a decorator to wrap stdout and send it as a message to hipchat

---
+++ hipchat/decorators.py
@@ -0,0 +1,31 @@
+import sys
+from hipchat import HipChat
+from cStringIO import StringIO
+
+
+class stdout_as_message(object):
+
+    def __init__(self, token, room_id, message_from, **kwargs):
+        """Init and set vars"""
+        self.token = token
+        self.room_id = room_id
+        self.message_from = message_from
+        self.kwargs = kwargs
+
+    def __call__(self, function):
+        """Call the function"""
+
+        def wrapped_function(*args, **kwargs):
+
+            try:
+                sys.stdout = StringIO()
+                o = function(*args,**kwargs)
+                out = sys.stdout.getvalue()
+                messager = HipChat(self.token)
+                messager.message_room(message=out, room_id=self.room_id, message_from=self.message_from, **self.kwargs)
+            finally:
+                sys.stdout.close()
+                sys.stdout = sys.__stdout__
+            return o
+
+        return wrapped_function
